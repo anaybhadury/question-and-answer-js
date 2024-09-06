dc0b46dd50258c31f78e318646af266e2ee15375
anaybhadury <bhadury9292@gmail.com>
2024-09-06 18:55:15 +0600

    added practice task 1

---
+++ practice-1.js
@@ -0,0 +1,20 @@
+/**
+ * write a first name
+ * write a last name
+ * write a first name plus last name
+ * write a fullname
+ * write a fullname length
+ * write a fullname uppercase
+ * write a fullname lowercase
+ */
+
+
+var firstName = "Anay"
+var lastName = " Bhadury"
+
+var fullName = firstName + lastName;
+
+console.log(firstName + lastName);
+console.log(fullName.length);
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());
