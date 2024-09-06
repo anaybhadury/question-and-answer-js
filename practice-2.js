47fa85461f38193547d9448e1f137b40d1395d8f
anaybhadury <bhadury9292@gmail.com>
2024-09-06 18:59:21 +0600

    added practice task 2

---
+++ practice-2.js
@@ -0,0 +1,61 @@
+/**
+ * Write a any number of sum, sub, multiply, divide, modulas and etc
+ * Fahrenheight to celsius convert
+ * celsius to farenheight
+ * Hour to Second Convert
+ * Degree celsius to Farenheit
+ */
+
+// Write a any number of sum, sub, multiply, divide, modulas and etc
+
+// let num1 = prompt("Enter first number: " );
+// let num2 = prompt("Enter last number: " );
+
+// num1 = parseInt(num1, 10);
+// num2 = parseInt(num2, 10);
+
+// let result;
+// result = num1 + num2;
+// console.log(num1 + ' + ' + num2 + ' = ' + result);
+
+// result = num1 - num2;
+// console.log(num1 + ' - ' + num2 + ' = ' + result);
+
+// result = num1 * num2;
+// console.log(num1 + ' * ' + num2 + ' = ' + result);
+
+// result = num1 / num2;
+// console.log(num1 + ' / ' + num2 + ' = ' + result);
+
+// result = num1 % num2;
+// console.log(num1 + ' % ' + num2 + ' = ' + result);
+
+// Fahrenheight to celsius convert
+
+// let Fahrenheight = parseFloat(prompt('Enter fahrenheight: '));
+// let celsius = (Fahrenheight - 32) * (5/9);
+
+// console.log('celsius = ', celsius);
+
+
+// celsius to farenheight
+
+// let cels = parseFloat(prompt('Enter Celsius: '));
+// let farn = (cels * (9/5)) + 32;
+
+// console.log('Farenheight = ', farn);
+
+// Hour to Second Convert
+
+// let hour = prompt('Enter the hour: ');
+// let second = number(hour) * 3600;
+
+// console.log(second);
+
+
+// Degree celsius to Farenheit
+
+let degree = prompt('Enter the degree: ');
+let farn = (Number(degree) * (9/5)) + 32;
+
+console.log(farn);
