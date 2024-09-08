5e46c55e755a6f938932b79a27e8c4ca8a500ee0
anaybhadury <bhadury9292@gmail.com>
2024-09-08 20:30:07 +0600

    added practice task 6

--- practice-5.js
+++ practice-5.js
@@ -1,7 +1,6 @@
 /**
  * WRITE A PROGRAM THAT WILL PRINT SUM OF ALL THE NUMBERS THAT ARE DIVISIBLE BY 3 AND 5 FROM 1-100. 
  */
-
 let sum = 0;
 
 for(let i = 1; i <= 100; i++){
---
+++ practice-6.js
@@ -0,0 +1,18 @@
+/**
+ * CREATE AN IIFES THAT PRINT SUM OF 2 NUMBERS
+ */
+(function (num1, num2){
+    let sum = num1 + num2;
+    console.log('the total sum is: ', sum);
+}
+)
+(5, 5);
+
+/**
+ * CREATE AN IIFES THAT PRINT SUM OF 3 NUMBERS
+ */
+(function(x, y, z){
+    let sum = x + y +z;
+    console.log('the total sum is: ', sum);
+})
+(5, 5, 5);
